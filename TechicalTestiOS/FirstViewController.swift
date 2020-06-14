ccab55876066c098c49e980f80a45d6047c8f645
fawwazmayda <maydafawwaz@gmail.com>
2020-06-14 14:47:47 +0800

    Initial Commit

---
+++ TechicalTestiOS/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  TechicalTestiOS
+//
+//  Created by Muhammad Fawwaz Mayda on 14/06/20.
+//  Copyright © 2020 Muhammad Fawwaz Mayda. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
