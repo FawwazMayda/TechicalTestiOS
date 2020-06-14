cb26fcd3b59d12d26077aadd4912d5bf22474811
fawwazmayda <maydafawwaz@gmail.com>
2020-06-14 17:32:42 +0800

    making writer custom dell and VC

---
+++ TechicalTestiOS/CustomCell/KaryaTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  KaryaTableViewCell.swift
+//  TechicalTestiOS
+//
+//  Created by Muhammad Fawwaz Mayda on 14/06/20.
+//  Copyright © 2020 Muhammad Fawwaz Mayda. All rights reserved.
+//
+
+import UIKit
+
+class KaryaTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
